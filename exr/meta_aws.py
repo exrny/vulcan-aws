e78c264f71c59ee3e0917cfae8b6d55a1148d91f
Peter Salnikov <peter@salnikov.cc>
2018-04-06 00:19:43 +0300

    Version updated to 0.1.6

--- exr/meta_aws.py
+++ exr/meta_aws.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 __license__ = "MIT License"
 __website__ = "https://code.exrny.com/opensource/exr-aws/"
 __download_url__ = ('https://github.com/exrny/exr-aws/archive/'
