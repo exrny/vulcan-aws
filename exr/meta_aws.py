df832f553bdd04674d8f1632171556ad7ffe3a00
Peter Salnikov <peter@salnikov.cc>
2018-06-10 16:53:26 +0300

    Version updated to 0.1.8

--- exr/meta_aws.py
+++ exr/meta_aws.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.7"
+__version__ = "0.1.8"
 __license__ = "MIT License"
 __website__ = "https://code.exrny.com/opensource/exr-aws/"
 __download_url__ = ('https://github.com/exrny/exr-aws/archive/'
