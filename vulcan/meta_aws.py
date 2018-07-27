232bbabe66abaca864121521c787ff204e5dc49e
Peter Salnikov <peter@salnikov.cc>
2018-07-27 20:48:37 +0300

    rename exr-aws to vulcan-aws

--- vulcan/meta_aws.py
+++ vulcan/meta_aws.py
@@ -1,5 +1,5 @@
-__version__ = "0.1.8"
+__version__ = "0.2.0"
 __license__ = "MIT License"
-__website__ = "https://code.exrny.com/opensource/exr-aws/"
-__download_url__ = ('https://github.com/exrny/exr-aws/archive/'
+__website__ = "https://code.exrny.com/opensource/vulcan-aws/"
+__download_url__ = ('https://github.com/exrny/vulcan-aws/archive/'
                     '{}.tar.gz'.format(__version__))
