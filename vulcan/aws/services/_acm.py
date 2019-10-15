0b052d8fdaf48f7f244ffb9f5b6a52bd2baaf251
Peter Salnikov <peter@salnikov.cc>
2019-10-16 01:32:53 +0300

    remove unused methods

--- vulcan/aws/services/_acm.py
+++ vulcan/aws/services/_acm.py
@@ -32,35 +32,3 @@ class AWSACM(AWSSession):
                             raise Exception('Multiple certificates ''with same domain name.')
                         else:
                             cert_arn = cert['CertificateArn']
-
-    def request_via_dns(self, **kwargs):
-        if 'domain_name' not in kwargs:
-            raise Exception('Argument missing: domain_name')
-
-        if 'alternative_names' not in kwargs:
-            raise Exception('Argument missing: alternative_names')
-
-        client = self.client('acm')
-
-        resp = client.request_certificate(
-            DomainName=kwargs.get('domain_name'),
-            SubjectAlternativeNames=kwargs.get('alternative_names'),
-            ValidationMethod='DNS',
-            IdempotencyToken=uuid.uuid4()
-        )
-
-        return resp
-
-    def get_dns_validation_options(self, **kwargs):
-        if 'certificate_arn' not in kwargs:
-            raise Exception('Argument missing: certificate_arn')
-
-        client = self.client('acm')
-
-        resp = client.describe_certificate(
-            CertificateArn=certificate_arn
-        )
-
-        return resp.get('Certificate') \
-                   .get('DomainValidationOptions')[0] \
-                   .get('ResourceRecord')
