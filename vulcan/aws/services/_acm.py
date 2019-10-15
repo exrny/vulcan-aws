89b4dc4c98b567b5e62d6ba26a643139f059756b
Peter Salnikov <peter@salnikov.cc>
2019-10-16 01:20:05 +0300

    method added: AWSACM.find_cert_arn

--- vulcan/aws/services/_acm.py
+++ vulcan/aws/services/_acm.py
@@ -11,6 +11,28 @@ class AWSACM(AWSSession):
             self
         ).__init__(kwargs['profile_name'])
 
+    def find_cert_arn(self, **kwargs):
+        cert_arn = None
+
+        if 'domain_name' not in kwargs:
+            raise Exception('Argument missing: domain_name')
+
+        client = self.client('acm')
+
+        paginator = client.get_paginator('list_certificates')
+
+        page_iterator = paginator.paginate(
+            CertificateStatuses=['ISSUED']
+        )
+        for page in page_iterator:
+            if 'CertificateSummaryList' in page:
+                for cert in page['CertificateSummaryList']:
+                    if cert['DomainName'] == kwargs['domain_name']:
+                        if cert_arn is not None:
+                            raise Exception('Multiple certificates ''with same domain name.')
+                        else:
+                            cert_arn = cert['CertificateArn']
+
     def request_via_dns(self, **kwargs):
         if 'domain_name' not in kwargs:
             raise Exception('Argument missing: domain_name')
