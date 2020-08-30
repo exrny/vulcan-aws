4aafa010905836c5b0fe33d6c7418b2b1b31cac9
Peter Salnikov <peter@salnikov.cc>
2020-08-30 11:42:09 +0300

    Version updated to 0.2.40

--- vulcan/meta_aws.py
+++ vulcan/meta_aws.py
@@ -1,4 +1,4 @@
-__version__ = "0.2.39"
+__version__ = "0.2.40"
 __license__ = "MIT License"
 __website__ = "https://code.exrny.com/opensource/vulcan-aws/"
 __download_url__ = ('https://github.com/exrny/vulcan-aws/archive/'
